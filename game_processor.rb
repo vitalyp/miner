f1637d7182b79543a88548625120d276e9823cb2
ariekdev <ariekdev@gmail.com>
2015-07-22 15:23:21 +0300

    enchanced with multi-session persisted games instances, managed by MinerGameContainer class

--- game_processor.rb
+++ game_processor.rb
@@ -1,32 +1,49 @@
-module GameConstants
+# Data Module, Miner config
+module MinerGameConfig
+  DEFAULTS = { map_rows: 10, map_cols: 10, map_bombs: 10, map_cell_px: 32   }
+  MAP_MAX_COLS = 30
+  MAP_MAX_ROWS = 30
   CELL = { empty: 0, mine: -1 }
-  MAP_RANGE = (1..30)
 end
 
-class GameProcessor
-  attr_accessor :rows
-  attr_accessor :cols
-  attr_accessor :bombs
-  attr_accessor :map
+# Contains and Produces isolated Game entities
+class MinerGameContainer
+  MAX_DATA_ITEMS = 100 # INFO: noMemory limitation fix in perspective
+  @@data = []
+  def self.find_or_create_by_sess_key(sess_key)
+    game = @@data.find{|g| g.sess_key == sess_key}
+    game = MinerGameProcessor.new(sess_key) unless game
+    game
+  end
+end
 
+# Game processor is a multy-session, persisted instance.
+# takes control
+# It contains isolated Game entity, and responsible for it
+class MinerGameProcessor
+  attr_accessor(:rows, :cols, :bombs, :map, :sess_key)
+
+  def initialize(sess_key)
+    @sess_key == sess_key
+  end
   # Construct new game map with specified attributes:
-  # <r> - map rows count
-  # <c> - map columns count
-  # <b> - mines count per map
-  #
+  # <r> - map rows count, <c> - map columns count, <b> - mines count per map
   def init_game(r, c, b)
     attr_exists =  !!(r && c && b)
 
-    attr_range_match = GameConstants::MAP_RANGE.include?(r) && GameConstants::MAP_RANGE.include?(c) && (1..r*c-1).include?(b)
+    map_rows_range = (1..MinerGameConfig::MAP_MAX_ROWS)
+    map_cols_range = (1..MinerGameConfig::MAP_MAX_COLS)
+    attr_range_match = map_rows_range.include?(r) && map_cols_range.include?(c) && r*c>1 && (1..r*c-1).include?(b)
+
     raise '[ERROR:GameProcessor] Initialization parameters are not correct' unless attr_exists
     raise '[ERROR:GameProcessor] Init params out of range ' unless attr_range_match
 
     @rows, @cols, @bombs = r, c, b
     @map = Array.new(@rows*@cols){ |index| index}
     @bombs.times do
-      free_indexes = @map.collect{|i|i!=GameConstants::CELL[:mine]}
+      free_indexes = @map.collect{|i|i!=MinerGameConfig::CELL[:mine]}
       rand_index = rand%free_indexes.size
-      @map[rand_index] = GameConstants::CELL[:mine]
+      @map[rand_index] = MinerGameConfig::CELL[:mine]
     end
   end
 
--- miner.rb
+++ miner.rb
@@ -1,23 +1,18 @@
+# Miner implementation # Game server
 require 'webrick'
 require './game_processor'
-server = WEBrick::HTTPServer.new(:Port=>8080)
-g_processor = GameProcessor.new
+game_server = WEBrick::HTTPServer.new(Port: 8080)
 
-DEFAULT_HEIGHT = 10
-DEFAULT_WIDTH = 10
-DEFAULT_BOMBS = 10
-DEFAULT_ZOOM = 32
+# define default game attributes
+miner_game_h = MinerGameConfig::DEFAULTS[:map_rows]
+miner_game_w = MinerGameConfig::DEFAULTS[:map_cols]
+miner_game_b = MinerGameConfig::DEFAULTS[:map_bombs]
+miner_game_z = MinerGameConfig::DEFAULTS[:map_cell_px]
 
 # preload network resources
 main_css = IO.read('main.css')
 ext_css = IO.read('ext.css')
 favicon = IO.read('favicon.ico')
-
-# define default game attributes
-miner_game_h = DEFAULT_HEIGHT
-miner_game_w = DEFAULT_WIDTH
-miner_game_b = DEFAULT_BOMBS
-miner_game_z = DEFAULT_ZOOM
 
 # deprecated?
 new_game_path = Proc.new do |h,w,b,z|
@@ -29,7 +24,7 @@ new_game_path = Proc.new do |h,w,b,z|
 end
 b_env = binding
 
-server.mount_proc('/'){ |req, resp|
+game_server.mount_proc('/'){ |req, resp|
   case req.path
     when '/favicon.ico'
       resp['Content-Type'] = 'image/x-icon'
@@ -40,7 +35,10 @@ server.mount_proc('/'){ |req, resp|
       miner_game_w = (req.query["w"] || DEFAULT_WIDTH).to_i
       miner_game_b = (req.query["b"] || DEFAULT_BOMBS).to_i
       miner_game_z = (req.query["z"] || DEFAULT_ZOOM).to_i
-      g_processor.init_game(miner_game_h, miner_game_w, miner_game_b)
+
+      session_key = req['session_key']
+      game = MinerGameContainer.find_or_create_by_sess_key(session_key)
+      game.init_game(miner_game_h, miner_game_w, miner_game_b)
 
       resp['Content-Type'] = 'text/html'
       resp.body = ERB.new(IO.read('index.html.erb')).result(b_env)
@@ -52,4 +50,4 @@ server.mount_proc('/'){ |req, resp|
       resp.body = ERB.new(IO.read('index.html.erb')).result(b_env)
   end
 }
-server.start
+game_server.start
